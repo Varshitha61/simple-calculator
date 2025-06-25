a5bed721d79b81b20d962ccaa08532ae9dd7682f
Varshitha61 <varshithasomashekar22@gmail.com>
2025-06-25 15:56:28 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,22 @@
+function appendValue(val) {
+    document.getElementById("display").value += val;
+}
+
+function clearDisplay() {
+    document.getElementById("display").value = "";
+}
+
+function backSpace() {
+    let current = document.getElementById("display").value;
+    document.getElementById("display").value = current.slice(0, -1);
+}
+
+function calculate() {
+    try {
+        let result = eval(document.getElementById("display").value);
+        document.getElementById("display").value = result;
+    }
+    catch {
+        alert("Invalid expression");
+    }
+}
